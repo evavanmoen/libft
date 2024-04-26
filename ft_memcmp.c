20a4fda821b2c2ed3faeb15b15054a30f9fdb078
Eva Gutierrez Vanmoen <evgutier@c3r8s4.42madrid.com>
2024-04-26 15:07:09 +0200

    updated ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: evgutier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 18:23:26 by evgutier          #+#    #+#             */
-/*   Updated: 2024/04/08 18:45:53 by evgutier         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:03:21 by evgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,13 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	ptr_s1 = (unsigned char *) s1;
 	ptr_s2 = (unsigned char *) s2;
-	if (n == 0)
-		return (0);
-	while (n-- && *ptr_s1 && *ptr_s2)
+	if (n != 0)
 	{
-		if (*ptr_s1 == *ptr_s2)
+		while (--n != 0)
 		{
-			ptr_s1++;
-			ptr_s2++;
+			if (*ptr_s1++ != *ptr_s2++)
+				return (*--ptr_s1 - *--ptr_s2);
 		}
-		else
-			return (*ptr_s1 - *ptr_s2);
-		printf("veo n: %zu\n", n);
 	}
-	if (n == 1)
-		return (0);
-	return (*ptr_s1 - *ptr_s2);
+	return (0);
 }
