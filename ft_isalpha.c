9b8e01ddeebbdf9a5e239096376dc74e3ffb873e
Eva Gutierrez Vanmoen <evgutier@c1r15s2.42madrid.com>
2025-03-09 14:44:05 +0100

    updated ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: evgutier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 14:17:24 by evgutier          #+#    #+#             */
-/*   Updated: 2024/03/21 18:44:53 by evgutier         ###   ########.fr       */
+/*   Updated: 2025/03/09 14:40:11 by evgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z')
+		return (1);
+	if ((unsigned char)c >= 'a' && (unsigned char)c <= 'z')
 		return (1);
 	return (0);
 }
